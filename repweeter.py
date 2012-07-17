fbb779a253a4a9915017d9aa8b3353c8b2b766a3
Ashkan Soltani <asoltani@cobalt.local>
2012-07-17 14:40:51 -0400

    prototype

---
+++ repweeter.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import twitter
+import re
+import urllib2
+import argparse
+from collections import defaultdict
+from operator import itemgetter
+
+parser = argparse.ArgumentParser(description="Count repeated tweets URLs")
+parser.add_argument("-u", "--user", type=str, help="Twitter username")
+parser.add_argument("-c", "--count", type=str, help="How many tweets to pull (max 200)")
+args = parser.parse_args()
+
+api = twitter.Api();
+dup = defaultdict(int)
+buf = []
+
+ss = api.GetUserTimeline(args.user, count=args.count, include_entities='expanded_url')
+for s in reversed(ss):
+ if (s.in_reply_to_user_id is None and not re.search(r'MT|RT', s.text)):
+	for u in s.urls:
+		url = ''
+		try:
+			lurl = urllib2.urlopen('http://therealurl.appspot.com?url=' + u.expanded_url).read()
+			url = re.match("(http[^\?]*)", lurl).group(0)
+		except urllib2.URLError, e:
+			pass
+			
+		url = url if url is not None else u
+		text = s.text.replace(u.url,url)
+		dup[url] += 1 
+		buf.append("(%s) %s | %s" % (dup[url], s.created_at[0:16], text.decode('utf-8')))
+		
+
+print "###############################################################################"
+print "### TOP REPWEETS:"
+c = 0
+for t in sorted(dup.items(), key=itemgetter(1), reverse=True):
+	print "(%s) %s" % (t[1], t[0])
+	c += 1
+	if c >= 10:
+		break
+
+print "\n\n"
+print "###############################################################################"
+print "### ACTUAL TWEETS (and REPWEETS):"
+for line in reversed(buf):
+	print line
+
